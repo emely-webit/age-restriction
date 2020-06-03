25d9b55aabdcaea1b0ff932172d1118b9f15155f
Emely <emelybuchholt@gmail.com>
2020-06-03 10:44:10 +0200

    Balloons finish

--- src/components/Diskotek/Diskotek18.js
+++ src/components/Diskotek/Diskotek18.js
@@ -2,22 +2,37 @@ import React from 'react'
 import {ReactComponent as Balloon} from '../Icons/Balloon.svg';
 import { SvgIcon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-// import { positions } from '@material-ui/system';
 
 const useStyles = makeStyles({
-    // mainPosition:{
-    //     position: "relative",
-    // },
+    mainPosition:{
+        position: "relative",
+        // width: "95%",
+        // margin: "0 auto"
+    },
     iconBalloon: {
         overflow: "unset",
+        height: "10px",
+        top: 0,
+        position: "absolute",
     },
-    // positionBallon:{
-    //     position: "absolute",
-    // },
-    // balloonPosition1:{
-    //     top: 0,
-    //     left: "10%"
-    // }
+    balloonPosition:{
+        left: 0
+    },
+    balloonPosition1:{
+        left: "8%"
+    },
+    balloonPosition2:{
+        left: "41%"
+    },
+    balloonPosition3:{
+        left: "51%"
+    },
+    balloonPosition4:{
+        left: "88%"
+    },
+    balloonPosition5:{
+        right: "2%"
+    },
 })
 
 
@@ -26,12 +41,13 @@ function Diskotek18() {
     const classes = useStyles();
 
     return (
-        <main position="relative">
-            {/* <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.positionBallon}`} color="primary" /> */}
-            {/* <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.positionBallon} ${classes.balloonPosition1}` } color="secondary" /> */}
-            <SvgIcon component={Balloon}  color="secondary" className={classes.iconBalloon} position="absolute"
-        top={40} />
-            {/* <Balloon color="secondary"/> */}
+        <main className={classes.mainPosition}>
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition}`} color="primary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition1}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition2}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition3}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition4}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition5}` } color="secondary" />
         </main>
     )
 }
