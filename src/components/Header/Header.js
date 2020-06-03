52cebf1cace2e9541401be9018a0d5ba6c84b130
dani690e <54890127+dani690e@users.noreply.github.com>
2020-06-03 10:28:42 +0200

    Header
    Tilføjelse af header med en drawer, som indeholder simpel information.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {makeStyles, AppBar, Toolbar, Typography, Button} from '@material-ui/core'
-import {ReactComponent as MenuIcon} from '../Icons/MenuIcon.svg';
+import React, { useState } from 'react';
+import { makeStyles, AppBar, Toolbar, Typography, Button, Drawer, Box } from '@material-ui/core'
+import { ReactComponent as MenuIcon } from '../Icons/MenuIcon.svg';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,10 +9,18 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    drawer: {
+        width: 250,
+    }
 }));
 
 export default function Header() {
     const classes = useStyles();
+    const [open, setOpen] = useState(false)
+
+    const handleDrawer = () => {
+        setOpen(!open)
+    }
 
     return (
         <div className={classes.root}>
@@ -21,11 +29,17 @@ export default function Header() {
                     <Typography variant="h6" className={classes.title}>
                         Age Restriction
                         </Typography>
-                    <Button edge="end" color="inherit" aria-label="menu" disableRipple>
+                    <Button edge="end" color="inherit" aria-label="Open information" onClick={handleDrawer}>
                         <MenuIcon />
                     </Button>
                 </Toolbar>
             </AppBar>
+            <Drawer anchor="right" open={open} onClose={handleDrawer}>
+                <Box className={classes.drawer} m={2}>
+                    <Typography variant="h4" align="center">Information</Typography>
+                    <Typography variant="body1" align="center">Meningen med dette projekt er meningen at gøre det enkelt at tjekke folks alder, i forhold til de danske regler når det angår aldersgrænser.</Typography>
+                </Box>
+            </Drawer>
         </div>
     );
 }
