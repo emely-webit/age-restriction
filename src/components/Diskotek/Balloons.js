2d23784bd6fe18e845320940e2837ce2cbf853bc
Emely <emelybuchholt@gmail.com>
2020-06-03 10:56:24 +0200

    component to balloons

---
+++ src/components/Diskotek/Balloons.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {ReactComponent as Balloon} from '../Icons/Balloon.svg';
+import { SvgIcon } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    mainPosition:{
+        position: "relative"
+    },
+    iconBalloon: {
+        overflow: "unset",
+        height: "10px",
+        top: 0,
+        position: "absolute",
+    },
+    balloonPosition:{
+        left: 0
+    },
+    balloonPosition1:{
+        left: "8%"
+    },
+    balloonPosition2:{
+        left: "41%"
+    },
+    balloonPosition3:{
+        left: "51%"
+    },
+    balloonPosition4:{
+        left: "88%"
+    },
+    balloonPosition5:{
+        right: "2%"
+    },
+})
+
+
+function Balloons() {
+
+    const classes = useStyles();
+
+    return (
+        <aside className={classes.mainPosition}>
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition}`} color="primary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition1}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition2}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition3}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition4}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition5}` } color="secondary" />
+        </aside>
+    )
+}
+
+export default Balloons
+ 
--- src/components/Diskotek/Diskotek18.js
+++ src/components/Diskotek/Diskotek18.js
@@ -1,53 +1,25 @@
 import React from 'react'
-import {ReactComponent as Balloon} from '../Icons/Balloon.svg';
-import { SvgIcon } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import Balloons from './Balloons'
+// import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-    mainPosition:{
-        position: "relative",
-        // width: "95%",
-        // margin: "0 auto"
-    },
-    iconBalloon: {
-        overflow: "unset",
-        height: "10px",
-        top: 0,
-        position: "absolute",
-    },
-    balloonPosition:{
-        left: 0
-    },
-    balloonPosition1:{
-        left: "8%"
-    },
-    balloonPosition2:{
-        left: "41%"
-    },
-    balloonPosition3:{
-        left: "51%"
-    },
-    balloonPosition4:{
-        left: "88%"
-    },
-    balloonPosition5:{
-        right: "2%"
-    },
-})
+// const useStyles = makeStyles({
+    
+// })
 
 
 function Diskotek18() {
 
-    const classes = useStyles();
+    // const classes = useStyles();
 
     return (
-        <main className={classes.mainPosition}>
-            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition}`} color="primary" />
+        <main>
+            <Balloons/>
+            {/* <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition}`} color="primary" />
             <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition1}` } color="secondary" />
             <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition2}` } color="secondary" />
             <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition3}` } color="secondary" />
             <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition4}` } color="secondary" />
-            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition5}` } color="secondary" />
+            <SvgIcon component={Balloon} className={`${classes.iconBalloon} ${classes.balloonPosition5}` } color="secondary" /> */}
         </main>
     )
 }
