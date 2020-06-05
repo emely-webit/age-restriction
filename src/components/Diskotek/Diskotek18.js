3e2ae47ee4a40068cb98271e5fbb8c72007a90d9
Emely <emelybuchholt@gmail.com>
2020-06-05 10:07:45 +0200

    diskotek virker wuhuu

--- src/components/Diskotek/Diskotek18.js
+++ src/components/Diskotek/Diskotek18.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import Balloons from './Balloons'
 import DiskotekInfo from './DiskotekInfo'
-import PopUp from './PopUp';
+// import PopUp from './PopUp';
 
 function Diskotek18() {
 
     let headlineTitle = "18+";
 
-    const allowedToOptions = ["Personen må IKKE komme ind", "Personen må komme ind, Men må IKKE købe alkohol eller tobak", "Personen må købe alkohol og tobak"];
+    // const allowedToOptions = ["Personen må IKKE komme ind", "Personen må komme ind, Men må IKKE købe alkohol eller tobak", "Personen må købe alkohol og tobak"];
 
-    // Alle nedenfor skal være dynamisk
-    let stageTitle = "Godkendt";
-    let age = "18";
-    let name = "Emely Buchholt";
-    let backgroundColor = "success.main";
-    let allowedTo = allowedToOptions[2];
+    // // Alle nedenfor skal være dynamisk
+    // let stageTitle = "Godkendt";
+    // let age = "18";
+    // let name = "Emely Buchholt";
+    // let backgroundColor = "success.main";
+    // let allowedTo = allowedToOptions[2];
 
     return (
         <main>
             <Balloons/>
             <DiskotekInfo headlineTitle={headlineTitle} />
-            <PopUp stageTitle={stageTitle} age={age} name={name} backgroundColor={backgroundColor} allowedTo={allowedTo} />
+            {/* <PopUp stageTitle={stageTitle} age={age} name={name} backgroundColor={backgroundColor} allowedTo={allowedTo} /> */}
         </main>
     )
 }
